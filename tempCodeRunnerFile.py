f4b853a38c8efd5def9174739a6c1ebb3122d292
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-02-03 00:52:41 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+pygame.draw.line(display, color, (pos[0] + l/2, pos[1]), (pos[0] + l, pos[1]), 4)
+    # pygame.draw.line(display, color, (pos[0], pos[1] + l/2), (pos[0], pos[1] + l), 4)
+    # pygame.draw.line(display, color, (pos[0] - l/2, pos[1]), (pos[0] - l, pos[1]), 4)
